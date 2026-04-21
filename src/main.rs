8687de745d7244e8f720f9c82399d5fff0c5bc18
prasdud <prasdudwastaken@gmail.com>
2026-04-21 19:41:47 +0530

    feat: slightly better web server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use axum::{routing::post, Json, Router};
+use axum::{routing::post, routing::get, Json, Router};
 use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
 
 #[derive(Deserialize)]
 struct Input {
@@ -17,9 +18,32 @@ async fn process(Json(payload): Json<Input>) -> Json<Output> {
     })
 }
 
+async fn encrypt(Json(payload): Json<Input>) -> Json<Output> {
+    Json(Output {
+        result: "this route will take an image and some plaintext and hide the plaintext in the image".to_string(),
+    })
+}
+
+async fn decrypt(Json(payload): Json<Input>) -> Json<Output> {
+    Json(Output {
+        result: "this route will take an image and decrpt the hidden ciphertext".to_string(),
+    })
+}
+
+async fn health() -> Json<Output> {
+    Json(Output {
+        result: OffsetDateTime::now_utc().to_string()
+    })
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/process", post(process));
+    let app = Router::new()
+        .route("/health", get(health))
+        .route("/process", post(process))
+        .route("/encrypt", post(encrypt))
+        .route("/decrypt", post(decrypt));
+        
     let listener = tokio::net::TcpListener::bind("localhost:5555").await.unwrap();
     println!("running on localhost:5555");
     axum::serve(listener, app).await.unwrap();
